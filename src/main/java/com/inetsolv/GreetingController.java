329a01b5595a61e2760bff7ed858fee1863cce60
Hammu786 <rehmanmsit@gmail.com>
2018-03-25 18:26:31 +0530

    test commit

---
+++ src/main/java/com/inetsolv/GreetingController.java
@@ -0,0 +1,35 @@
+package com.inetsolv;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class GreetingController{
+	
+	@Autowired
+	Environment env;
+
+	@RequestMapping("/greet")
+	Greet greet(){
+		return new Greet("Hello World!");
+	}
+	
+    @RequestMapping("/greeting")
+    @ResponseBody
+    public HttpEntity<Greet> greeting(@RequestParam(value = "name", required = false, defaultValue = "HATEOAS") String name) {
+    	Greet greet = new Greet("Hello " + env.getProperty("test"));
+        greet.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+        return new ResponseEntity<Greet>(greet, HttpStatus.OK);
+    }
+    
+}
